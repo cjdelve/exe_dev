4a581cea541dd8f983d6961b7cabe48c83c83d14
cjdelve <cdelve@students.mchenry.edu>
2017-10-08 19:29:04 -0500

    Delete splinter_exe.py

--- splinter_exe.py
+++
@@ -1,7 +0,0 @@
-from splinter import Browser
-
-executable_path = {'executable_path': 'C:/Users/cdelve/Desktop/chromedriver'}
-
-browser = Browser("chrome")
-
-browser.visit("https://www.google.com/")
